f8a310fd275f13904816685b34aa2bc7572a1501
Kenneth Reitz <me@kennethreitz.org>
2017-12-08 23:53:47 +0000

    initial model

--- richtext/core/models.py
+++ richtext/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class RichPost(models.Model):
+    text = models.TextField()
+    views = models.BigIntegerField()
+    published = models.DateTimeField('date published')
