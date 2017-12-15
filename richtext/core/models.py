dcdc0a9c06329cd06e35967ed3c130e8b5211e73
Kenneth Reitz <me@kennethreitz.org>
2017-12-15 15:53:14 +0000

    uuids

--- richtext/core/models.py
+++ richtext/core/models.py
@@ -1,7 +1,12 @@
+import uuid`
 from django.db import models
 
 # Create your models here.
 class RichPost(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.TextField()
     views = models.BigIntegerField()
     published = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return "<RichPost id='{}'>".format(self.id)
